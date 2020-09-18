b575310e9b6786a48a000024f124187f643c65bb
Akumu collins <akumucollins001@gmail.com>
2020-09-18 07:22:09 +0300

    initial commits

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-export MOVIE_API_KEY=d3fd8f785d72df0916d8d9932813a6b0
-export SECRET_KEY=eyJhbGciOiJIUzI1NiJ9
+# export MOVIE_API_KEY='d3fd8f785d72df0916d8d9932813a6b0'
+# export SECRET_KEY='eyJhbGciOiJIUzI1NiJ9'
+# export MAIL_USERNAME='akumucollins001@gmail.com'
+# export MAIL_PASSWORD='collinsomondi'
 python3.8 manage.py server
